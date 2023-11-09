0de43316d5f5b74efe90139eeca4563798abe917
shiüo <shiueo.csh@gmail.com>
2023-11-10 00:39:00 +0900

    curr

--- android/app/src/main/java/com/shiueo/supernet/VideoSene.kt
+++ android/app/src/main/java/com/shiueo/supernet/VideoSene.kt
@@ -1,11 +1,33 @@
 package com.shiueo.supernet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
 
 class VideoSene : AppCompatActivity() {
+    private lateinit var exit_btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_sene)
+
+        val URL = intent.getStringExtra("url")
+        val EMAIL = intent.getStringExtra("email")
+
+        val webView: WebView = findViewById(R.id.webview)
+
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl(URL!!)
+
+        exit_btn = findViewById(R.id.button7)
+        exit_btn.setOnClickListener {
+            val intent = Intent(this, supernet_scene::class.java)
+            intent.putExtra("email", EMAIL)
+            startActivity(intent)
+        }
     }
 }
--- android/app/src/main/java/com/shiueo/supernet/supernet_scene.kt
+++ android/app/src/main/java/com/shiueo/supernet/supernet_scene.kt
@@ -1,11 +1,15 @@
 package com.shiueo.supernet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class supernet_scene : AppCompatActivity() {
     private lateinit var acc_text: TextView
+    private lateinit var pikachu_btn: Button
+    private lateinit var goku_btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_supernet_scene)
@@ -15,5 +19,23 @@ class supernet_scene : AppCompatActivity() {
 
         acc_text.text = "You're logged in as " + EMAIL
 
+        pikachu_btn = findViewById(R.id.button)
+        goku_btn = findViewById(R.id.button2)
+
+        pikachu_btn.setOnClickListener {
+            val intent = Intent(this, VideoSene::class.java)
+            intent.putExtra("url", "https://youtube.com")
+            startActivity(intent)
+        }
+
+        goku_btn.setOnClickListener {
+            val intent = Intent(this, VideoSene::class.java)
+            intent.putExtra("url", "https://longdogechallenge.com/")
+            intent.putExtra("email", EMAIL)
+            startActivity(intent)
+        }
+
     }
+
+
 }
